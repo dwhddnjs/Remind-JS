2ab6b14ea009ed4d9d9418b88654c5ca6c319c87
이종원 <per1215@naver.com>
2021-06-08 23:27:30 +0900

    data type

---
+++ let.js
@@ -0,0 +1,19 @@
+let value = 1; // 변수
+
+value = 2;
+
+// 재할당 가능 재선언 불가능
+
+console.log(value);
+
+const a = 1; // 상수
+
+// 재할당 재선언이 불가능
+
+//데이터 타입
+let value = 100; // 숫자
+let text = "hello"; // 문자열
+let good = true //참
+let bad = false// 거짓
+let some = null // 없다
+let any = undefined // 아직 정하지 않음
